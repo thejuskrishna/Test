2de621b68911f70f4bcad90b496a13e91bab4aff
Thejus <tkrishna@zynga.com>
2013-10-08 16:29:53 +0530

    Tear down added

--- src/scripts/DataTest.java
+++ src/scripts/DataTest.java
@@ -8,6 +8,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import lib.Library;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -17,6 +18,7 @@ public class DataTest {
 
 	Library lib;
 	WebDriver driver;
+	String cleanup;
 	
 	@DataProvider(name="data")
 	public Object[][] init1() 
@@ -44,6 +46,7 @@ public class DataTest {
 	public void setup()
 	{
 		lib = new Library();
+		cleanup = "no";
 		
 	}
 	
@@ -51,9 +54,24 @@ public class DataTest {
 	public void TestingDataProvider(HashMap<String, String> xmlVal)
 	{
 		String browser =  xmlVal.get("browser");
+		cleanup = xmlVal.get("cleanup");
 		System.out.println("Browser received : " + browser);
 		driver = lib.startBrowser(browser);
+		driver.get("http://facebook.com");
 	}
 	
+	@AfterClass
+	public void tearDown()
+	{
+		if(cleanup.equalsIgnoreCase("yes"))
+		{
+			try {
+				Runtime.getRuntime().exec("taskkill -f -im chromedriver.exe");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 	
 }
