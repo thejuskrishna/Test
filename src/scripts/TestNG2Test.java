fb9ed2a27f9d85549003c82c251e0730de24d27e
thejuskrishna <thejuskrishna@gmail.com>
2015-03-06 13:26:55 +0530

    Select drop down added

--- src/scripts/TestNG2Test.java
+++ src/scripts/TestNG2Test.java
@@ -1,10 +1,14 @@
 package scripts;
 
+import java.util.List;
+
 import lib.Library;
 
 import org.testng.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -26,6 +30,9 @@ public class TestNG2Test {
 		driver.findElement(By.id("email")).sendKeys("");
 		driver.findElement(By.id("pass")).sendKeys("");
 		driver.findElement(By.id("u_0_b")).click();
+		WebElement ele = driver.findElement(By.id("month"));
+		Select se = new Select(ele);
+		se.selectByVisibleText("Feb");
 		System.out.println("Mouse Over Text :"+ driver.findElement(By.linkText("Developers")).getAttribute("title"));
 		Assert.assertEquals(driver.findElement(By.linkText("Developers")).getAttribute("title"), "Kopp");
 	}
@@ -33,7 +40,7 @@ public class TestNG2Test {
 	@AfterClass
 	public void teardown() {
 		
-		driver.close();
+//		driver.close();
 	}
 	
 }
